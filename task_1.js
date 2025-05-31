a62d6651aa2cecea9719c9426e60335a6742c611
Jignesh Ambaliya <23it002@charusat.edu.in>
2025-05-31 11:48:57 +0530

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,7 @@
+function handleAction(action) {
+    if (action === 'readMore') {
+        alert("Redirecting to detailed information...");
+    } else if (action === 'consult') {
+        alert("Redirecting to consultation form...");
+    }
+}
